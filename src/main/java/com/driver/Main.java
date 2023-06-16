8accf8ab7a2219d0d3ce1994aec90084b2065658
ash1505-lesha <ashleshamahajan766@gmail.com>
2023-06-17 01:56:18 +0530

    done Solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,27 @@
 package com.driver;
 
 public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        Product p= new Product();
+        p.product(1,2);
+        p.product(1,2,3);
+        p.product(1.0,2.0);
+    }
+
+    public static  class Product{
+
+        public int product(int x, int y){
+            return x*y;
+        }
+        public int product(int x, int y,int z){
+            return x*y*z;
+        }
+        public double product(double x, double y){
+            return x*y;
+        }
+    }
 
 }
--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -3,6 +3,8 @@ package com.driver.test;
 import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
+
+
 public class TestCases {
-     
+
 }
